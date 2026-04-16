ab2ff7825412a13634c955e32390929641caec00
shubhamsawant1 <shubhamsawant22011@gmail.com>
2026-04-16 11:54:13 +0530

    upadate

--- day2/area_of_square.py
+++
@@ -1,9 +0,0 @@
-# Question 1 :   in a program , input the side of a square. You have to output the area of the square
-# input: n(side)
-# output: n*n (area)
-
-a = int(input("Enter a side of square: "))
-
-area_of_square = a * a
- 
-print("area of square: ", area_of_square)
--- day2/avragemarks.py
+++
@@ -1,7 +0,0 @@
-english = int(input("Enter your English marks: "))
-science = int(input("Enter your Science marks: "))
-math = int(input("Enter your Math marks: "))
-
-avg = (english + science + math) / 3
-
-print("Avrage marks: ", avg)   
