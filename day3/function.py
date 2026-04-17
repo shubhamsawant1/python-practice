9218fc88a035c237e57614ae0764c87fb1c4275f
shubhamsawant1 <shubhamsawant22011@gmail.com>
2026-04-17 14:56:16 +0530

    update

---
+++ day3/function.py
@@ -0,0 +1,15 @@
+
+
+
+def main():
+   
+    # print * number of times
+
+    num = int(input("Enter the number of rows: "))
+
+    for i in range(1, num + 1):
+        print("*" * i)
+
+
+
+main()
---
+++ day3/samenumber.py
@@ -0,0 +1,6 @@
+num = 5
+
+for i in range(1, num + 1):
+    for j in range(i):
+        print("*", end=" ")
+    print()
